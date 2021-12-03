6516813f3466a3337ae2b9c5a3bcdd37d7c3c98c
Jocelyn Mejia <jossy.m.larios@gmail.com>
2021-12-03 08:38:27 -0800

    Completed day 2

--- Advent_Cal_2021/ComplexNavy.py
+++ Advent_Cal_2021/ComplexNavy.py
@@ -10,6 +10,7 @@ for i in range(0, len(navy)):
 
 forwardCord = 0
 depth = 0
+aim = 0
 
 sub1 = "forward"
 sub2 = "down"
@@ -18,10 +19,11 @@ sub3 = "up"
 for j in range(0, len(navy)):
     if sub1 in navy[j]:
         forwardCord += int(navy[j][1])
+        depth += (aim * int(navy[j][1]))
     elif sub2 in navy[j]:
-        depth += int(navy[j][1])
+        aim += int(navy[j][1])
     elif sub3 in navy[j]: 
-        depth -= int(navy[j][1])
+        aim -= int(navy[j][1])
 
 print("The Forward Coord is ")
 print(forwardCord)
